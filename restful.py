99e89ede6f4da9b23225fa04c8789ee2ce43727a
windstormeye <877302410@qq.com>
2017-12-04 14:04:49 +0800

    完成 GET API测试

---
+++ restful.py
@@ -0,0 +1,53 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask import jsonify  
+from flask import abort
+from flask import make_response
+
+import test
+
+app = Flask(__name__)
+api = Api(app)
+
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route('/')
+def index():
+    return 'Get out!🙂'
+
+@app.route('/dachuang/api/v1/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+@app.route('/dachuang/api/v1/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    isID = 0
+    for taskUnit in tasks:
+        if taskUnit['id'] == task_id:
+            isID = 1
+            return jsonify({'task':taskUnit})
+    if isID == 0:
+        abort(404);
+    else:
+        return unit;
+
+@app.errorhandler(404)
+def no_found(error):
+    return make_response(jsonify({'error':'Not found'}), 404)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True)
